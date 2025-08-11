2be19e8fa6de984288f65400ecdddd47cf969362
Ziqiao Tan <liness.0713@gmail.com>
2025-08-11 17:50:50 +0900

    去除废弃代码

--- app_bak/api/submit/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/submit/route.ts
-import { supabase } from "@/lib/supabaseClient";
-
-export async function POST(req: Request) {
-  const { name, message } = await req.json();
-  await supabase.from("contacts").insert({ name, message });
-  return new Response(JSON.stringify({ success: true }));
-}
